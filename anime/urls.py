53621df239004d5eb023a1ad0369815f73b943e9
Ganesh Godse <ganeshkgodse@gmail.com>
2026-01-30 14:32:29 +0530

    Added update and delete functionality

--- anime/urls.py
+++ anime/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('anime/list', views.show, name='animes'),
     path('anime/list/add', views.add, name='animes'),
     path('anime/list/find', views.find, name='animes'),
+    path('anime/list/update', views.update, name='animes'),
+    path('anime/list/delete', views.delete, name='animes'),
 ]
--- anime/views.py
+++ anime/views.py
@@ -19,3 +19,26 @@ def find(request):
 	json_format = json.loads(request.body.decode('utf-8'))
 
 	return HttpResponse( Anime.objects.filter(name = json_format["Name"]).values() )
+
+def update(request):
+	json_format = json.loads(request.body.decode('utf-8'))
+
+	if len( Anime.objects.filter(name=json_format["Name"]).values()) > 0:
+		for key in json_format.keys():
+			if key == "Synopsis":
+				Anime.objects.filter(name=json_format["Name"]).update(synopsis=json_format["Synopsis"])
+			if key == "Rating":
+				Anime.objects.filter(name=json_format["Name"]).update(rating=json_format["Rating"])
+
+	return HttpResponse( Anime.objects.filter(name = json_format["Name"]).values() )
+
+def delete(request):
+	json_format = json.loads(request.body.decode('utf-8'))
+
+	if len( Anime.objects.filter(name=json_format["Name"]).values()) > 0:
+		Anime.objects.filter(name=json_format["Name"]).delete()
+
+	return HttpResponse("Done")
+
+
+
