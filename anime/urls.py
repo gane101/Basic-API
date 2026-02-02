86bce156989069a8f33636459fbf7a3187687aa3
Ganesh Godse <121635221+gane101@users.noreply.github.com>
2026-02-02 18:08:35 +0530

    Updated

--- anime/urls.py
+++ anime/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('anime/list', views.show, name='animes'),
-    path('anime/list/add', views.add, name='animes'),
-    path('anime/list/find', views.find, name='animes'),
-    path('anime/list/update', views.update, name='animes'),
-    path('anime/list/delete', views.delete, name='animes'),
+    path('list', views.show, name='animes'),
+    path('list/add', views.add, name='animes'),
+    path('list/find', views.find, name='animes'),
+    path('list/update', views.update, name='animes'),
+    path('list/delete', views.delete, name='animes'),
 ]
